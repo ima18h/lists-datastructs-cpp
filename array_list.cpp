a796a97ec10fee506be5feae49c94eb5b4ec5fc2
Erling Pettersen Vollan <erlingpv@uio.no>
2021-10-25 14:41:54 +0200

    created file and started

---
+++ array_list.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class ArrayList {
+    private:
+        int capacity = 1; 
+        int *data ;
+
+    public:
+        int size = 0;
+        
+
+
+    int lenght(){
+        return size;
+        // return nb. of elements in the object;
+    }
+
+
+};
