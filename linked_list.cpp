7c40780711422612d5d3699ace7eb81e8f8307df
ima18h <imadha@math.uio.no>
2021-10-28 20:08:21 +0200

    Add LinkedList class and length() method

--- linked_list.cpp
+++ linked_list.cpp
@@ -4,8 +4,27 @@ struct Node {
     struct Node *next;
 };
 
+class LinkedList {
+private:
+    Node* head;
+    Node* tail;
+    int len;
+public:
+    LinkedList();
+    int length();
+};
 
-int main() {
+LinkedList::LinkedList() : head(nullptr), tail(nullptr), len(1)
+{
+}
+
+int LinkedList::length() {
+    return len;
+}
+
+
+
+int main(){
 
     return 0;
 }
