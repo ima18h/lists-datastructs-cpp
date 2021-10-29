f207dea85ff378ca231593950dbbe46eb85cd155
ima18h <imadha@math.uio.no>
2021-10-29 21:10:28 +0200

    Implement int& operator[](int), overloader

--- linked_list.cpp
+++ linked_list.cpp
@@ -81,11 +81,33 @@ LinkedList::~LinkedList() {
 int LinkedList::length() {
   return len;
 }
+int &LinkedList::operator[](int index) {
+  int half = int(len-1 / 2);
+  if(index >= len or index < 0) {
+    throw range_error("IndexError: Index out of range");
+  } else if(index > half){
+    int jumps = len - index;
+    Node* current = tail;
+    for (int i = 0; i < jumps; ++i) {
+      current = current->prev;
+    }
+    return current->data;
+  } else{
+    Node* current = head;
+    for (int i = 0; i < index; ++i) {
+      current = current->next;
+  }
+    return current->data;
+}
 
 // main for testing purposes -----------------------------------------------------------
 int main() {
   LinkedList list;
   list.length();
   cout << list.length() << endl;
+
+  for (int i = 0; i < 1; ++i) {
+    cout << i << "\n";
+  }
   return 0;
 }
